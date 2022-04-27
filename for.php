61e438b011ae9502c3e23114c3c694950a86f968
rapapo <asyraf.akif91k@gmail.com>
2022-04-28 00:18:51 +0800

    first commenr

---
+++ for.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+for (Scount = 1; $count <10; $count++) {
+    if ( (Scount # 3) -- 0) (
+        echo "Scount is divisible by 3!<br>";
+    }
+}
+?><
+</body>
+</html>

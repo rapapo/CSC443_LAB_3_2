890ac91ce478e420f21fd9d89ea8461f7de460b0
rapapo <asyraf.akif91k@gmail.com>
2022-04-28 00:22:05 +0800

    first comment

---
+++ stringg.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function
+add ($x, $y) {
+    $aum- $x+ $y;
+    return $sum;
+}
+
+$numl-11;
+$num2-15;
+$summ= add($numl, $num2)
+echo "$num1 + $num2 -" . $summ;
+?>
+</body>
+</html>

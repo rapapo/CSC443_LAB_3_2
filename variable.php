b99e4f7540125f5b479c71d84525a0c89b325a0a
rapapo <asyraf.akif91k@gmail.com>
2022-04-28 00:09:19 +0800

    first comment

---
+++ variable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+Susername "Fred Smith"
+echo $username;
+echo "<br />";
+Scurrent_user - Susername:
+echo $current_user;
+echo "<br />";
+Snuml
+10.5;
+echo $numl;
+?>
+</body>
+</html>
